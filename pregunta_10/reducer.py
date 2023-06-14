38689d10b14ea3ca28737a78b2992d35a1014924
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 00:52:06 -0500

    Entregafinal

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+    d = None
+    total = 0
+
+    for i in sys.stdin:
+        b, value = i.split("-")
+        value = value.strip()
+
+        if b == d:
+            n = n + ',' + str(int(value))
+        else:
+            if d is not None:
+                sys.stdout.write("{}\t{}\n".format(d, n))
+            d= b
+            n = str(int(value))
+
+    sys.stdout.write("{}\t{}\n".format(d, n))
