dd3eac2ea11ed8903a9108d56d019407ad0f5888
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:02:38 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,29 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    v_sum  = 0
+    v_count  = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+     
+            v_sum = v_sum + val  
+            v_count =  v_count + 1
+
+        else:
+         
+            if curkey is not None:
+            
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, v_sum, v_sum/v_count))
+
+            curkey = key
+            v_sum = val
+            v_count = 1
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, v_sum, v_sum/v_count))
