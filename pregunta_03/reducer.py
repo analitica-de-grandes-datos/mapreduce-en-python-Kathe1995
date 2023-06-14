edc32fe63c5b91995955d0005d1acb2424545fda
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 03:12:19 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -3,17 +3,8 @@
 #
 import sys
 
-#
-# Esta funcion reduce los elementos que tienen la misma clave
-#
 if __name__ == '__main__':
 
-    #
-    # cada linea de texto recibida es una entrada clave \tabulador valor
-    #
     for line in sys.stdin:
-
-        lista = line.split(",")
-
-
-        sys.stdout.write("{},{}\n".format(lista[1].strip(),lista[0]))
+        
+       sys.stdout.write("{},{}\n".format(line.split(",")[1].strip(),line.split(",")[0]))
