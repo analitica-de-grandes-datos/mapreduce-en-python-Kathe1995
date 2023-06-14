2344ad030256f2ae19ff73a4b278a10100fb3796
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 02:10:29 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    c = None
+    total = 0
+
+    for line in sys.stdin:
+
+        key, value = line.split("\t")
+        value = int(value)
+
+        if key == c:
+            total += value
+        else:
+            if c is not None:
+                sys.stdout.write("{},{}\n".format(c, total))
+
+            c = key
+            total = value
+
+    sys.stdout.write("{},{}\n".format(c, total))
