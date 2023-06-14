c91e4a72bb70fb7690c16a084ebc64ce73448560
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 03:10:59 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,11 +1,6 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+
 import sys
 
-#
-# Esta funcion reduce los elementos que tienen la misma clave
-#
 if __name__ == '__main__':
 
     curkey = None
