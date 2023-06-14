76e79a87559ab3cae18d63505063f36172e82d87
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 02:28:14 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -7,10 +7,10 @@ dict =  {}
 for row in sys.stdin:
   
   if row.strip().split(";")[0] in dict.keys():
-    dict[row.strip().split(";")[0][0]] +=  1
+    dict[row.strip().split(";")[0]] +=  1
     
   else:
-    dict[row.strip().split(";")[0][0]] = 1
+     dict[row.strip().split(";")[0]] = 1
 
 
 lista = [(i,dict[i]) for i in dict.keys()]
@@ -18,3 +18,6 @@ lista.sort(key=lambda x: x[0])
 
 for t in lista:
   sys.stdout.write(str(t[0]) + "," + str(t[1]) + "\n") 
+
+ 
+
