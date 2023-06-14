4fc4b86aa0f9981a62689ecfd950e95313ae51bb
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 02:37:52 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -3,24 +3,32 @@
 
 import sys
 
-dict =  {}
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
 
-for row in sys.stdin:
-  linea = row.strip()
-  linea = linea.split(";")
-  if linea[0] in dict.keys():
-    dict[linea[0]] +=  1
-    
-  else:
-    dict[linea[0]] = 1
+    curkey = None
+    total = 0
 
+   
+    for line in sys.stdin:
 
-lista = [(i,dict[i]) for i in dict.keys()]
-lista.sort(key=lambda x: x[0])
+        key, val = line.split("\t")
+        val = int(val)
 
-for tupla in lista:
-  
-  line =  str(tupla[0]) + "," + str(tupla[1]) + "\n"
-  sys.stdout.write(line) 
+        if key == curkey:
+          
+            total += val
+        else:
+            
+            if curkey is not None:
+              
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
  
 
