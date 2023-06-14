6b0cc6239388bf79e9e4f11539285a237aea5619
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:14:12 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,22 +1,25 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
-import sys
-
-if __name__ == '__main__':
-    d = None
-    total = 0
-
-    for i in sys.stdin:
-        b, value = i.split("-")
-        value = value.strip()
-
-        if b == d:
-            n = n + ',' + str(int(value))
-        else:
-            if d is not None:
-                sys.stdout.write("{}\t{}\n".format(d, n))
-            d= b
-            n = str(int(value))
-
-    sys.stdout.write("{}\t{}\n".format(d, n))
+import sys 
+ 
+if __name__ == '__main__': 
+ 
+    curkey = None 
+    total = 0 
+ 
+    for line in sys.stdin: 
+ 
+        key, val = line.split("\t") 
+        val = val.strip() 
+ 
+        if key == curkey: 
+          
+            n = n + ',' + str(int(val)) 
+        else: 
+         
+            if curkey is not None: 
+  
+                sys.stdout.write("{}\t{}\n".format(curkey, n)) 
+ 
+            curkey = key 
+            numeros = str(int(val)) 
+ 
+    sys.stdout.write("{}\t{}\n".format(curkey, n))
