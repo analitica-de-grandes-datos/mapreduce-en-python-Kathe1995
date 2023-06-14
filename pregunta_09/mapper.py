38b324b4dfcd1704a3be3b21baad502cd0c8aec5
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:07:59 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,5 +1,9 @@
 from operator import index
 import sys
 if __name__ == "__main__":
+
+
     for line in sys.stdin:
-        sys.stdout.write("{}   {}   {}\n".format( line.split('   ')[2].strip().zfill(4), str(line.split('   ')[0]), line.split('   ')[1]))
+      
+        sys.stdout.write("{}   {}   {}\n".format( line.split('   ')[2].strip().zfill(4) , str(line.split('   ')[0]), line.split('   ')[1]))
+
