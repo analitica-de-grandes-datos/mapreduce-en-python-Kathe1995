27cb0ea0cf112c8b0db4b72f9db534c321e55a7c
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:18:58 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -3,10 +3,10 @@
 import sys
 
 if __name__ == "__main__":
-    contador = 0
+    count = 0
 
     for line in sys.stdin:
         sys.stdout.write("{}   {}   {}\n".format(line.split('   ')[1], line.split('   ')[2].strip(), int(line.split('   ')[0]))) 
-        if contador >= 5: 
+        if count >= 5: 
             break 
-        contador += 1
+        count += 1
