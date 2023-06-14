526660774a65df095981d4f5c10c65c725904630
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 01:31:16 -0500

    ..

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys 
+
+
+for line in sys.stdin:
+       sys.stdout.write("{},{},{}\n".format(int(line.split(",")[1]),line.split(',')[0],int(line.split(",")[1])))
