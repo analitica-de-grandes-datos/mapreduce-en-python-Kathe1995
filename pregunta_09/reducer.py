700ef843fd19fd2932b23354602ff89559e0755f
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:06:09 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,11 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+
+import sys
+
+if __name__ == "__main__":
+    counts = 0
+    
+    for line in sys.stdin:
+        sys.stdout.write("{}   {}   {}\n".formatline.split('   ')[1],line.split('   ')[2].strip(), int(line.split('   ')[0]))) 
+        if counts >= 5: 
+            break 
+        counts += 1
