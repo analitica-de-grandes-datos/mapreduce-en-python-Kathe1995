c29079484ca5f52fc552caeb5e3951be88630a7d
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 03:31:45 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,4 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        sys.stdout.write("{},1\n".format(line.split(" ")[0]))
