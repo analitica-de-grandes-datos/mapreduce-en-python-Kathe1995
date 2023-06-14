1d19ca0d6bf2da83d72988770c7dbf0c0aafc637
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 02:03:26 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -7,7 +7,7 @@ list = []
 for row in sys.stdin:
   tupla = (row.strip().split(";")[0],row.strip().split(";")[1])
   list.append(tupla)
-  list.sort(lambda x: x[1])
+  list.sort(key=lambda x: x[1])
 for tupla in list:
   letra = tupla[0]
   line =  letra + "," + str(tupla[1]) + "\n"
