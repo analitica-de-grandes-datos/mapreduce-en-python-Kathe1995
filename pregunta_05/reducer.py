efb2bf665f468f1201b997bd60db259ead80d844
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 02:14:15 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == "__main__":
+
+    c = None
+    total = 0
+    for line in sys.stdin:
+
+        key, value = line.split(",")
+        value = int(value)
+
+        if key == c:
+            total += value
+        else:
+            if c is not None:
+                sys.stdout.write("{}\t{}\n".format(c, total))
+            c= key
+            total = value
+
+    sys.stdout.write("{}\t{}\n".format(c, total))
