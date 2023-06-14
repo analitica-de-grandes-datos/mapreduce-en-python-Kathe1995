5c4bf0a17683392f27093b10d83fdd145d7de6a5
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 01:51:12 -0500

    *

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -4,9 +4,9 @@
 import sys
 list = []
 
-for line in sys.stdin:
-  tupla=(line.strip().split(";")[0],line.strip().split(";")[1])
+for row in sys.stdin:
+  tupla=(row.strip().split(";")[0],row.strip().split(";")[1])
   list.append(tupla)
   list.sort(lambda k: k[1])
-for typla in list:
+for tupla in list:
   sys.stdout.write(tupla[0] + "," + str(tupla[1]) + "\n") 
