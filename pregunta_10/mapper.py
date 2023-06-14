49ead6fc8cd640927fa310e2c4ddf893dc56f183
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 04:12:47 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,10 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+if __name__ == "__main__":
+  
+    for line in sys.stdin:
+        key, val = line.split('\t') 
+        val =list(val.strip().split(',')) 
+        key = key.zfill(4) 
+        
+        for word in val: 
+            sys.stdout.write("{}\t{}\n".format(word ,key))
