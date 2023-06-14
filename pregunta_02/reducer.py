c66396ac2fac1eeaa6bd7587f3a8e046669f897c
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 03:26:23 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,1 +1,21 @@
+import sys
+if __name__ == '__main__':
 
+    curkey = None
+    value = 0
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            if val > value:
+                value = val
+        else:
+            if curkey is not None:
+         
+                sys.stdout.write("{}\t{}\n".format(curkey, value))
+
+            curkey = key
+            value = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, value))
