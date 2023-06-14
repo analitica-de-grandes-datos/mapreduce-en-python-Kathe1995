8c5ba84573b9ac755a306d0b9afd31884ed2cf53
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 01:44:27 -0500

    ....

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+l = []
+
+for line in sys.stdin:
+  l.append((line.strip().split(";")[0],line.strip().split(";")[1]))
+  l.sort(key=lambda k: k[1])
+for t in l:
+  sys.stdout.write(t[0] + "," + str(t[1]) + "\n") 
