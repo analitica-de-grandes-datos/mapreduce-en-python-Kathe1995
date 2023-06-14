34c7e6ec430ad108f283509b61747ed7bde2d254
Kathe1995 <126032325+Kathe1995@users.noreply.github.com>
2023-06-14 03:48:37 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,27 +1,30 @@
 import sys
+
 if __name__ == '__main__':
+
     curkey = None
-    value_max  = 0
+    v_max  = 0
+
     for line in sys.stdin:
 
         key, val = line.split("\t")
         val = float(val)
 
         if key == curkey:
-          
-            if val > value_max:
-                value_max = val
-            if val < value_min:
-                 value_min = val
+
+            if val > v_max:
+                v_max = val
+            if val < v_min:
+                v_min = val
 
         else:
-     
+         
             if curkey is not None:
-          
-                sys.stdout.write("{}\t{}\t{}\n".format(curkey, value_max, value_min))
+            
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, v_max, v_min))
 
             curkey = key
-            value_max = val
-            value_min = val
+            v_max = val
+            v_min = val
 
-    sys.stdout.write("{}\t{}\t{}\n".format(curkey, value_max, value_min))
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, v_max, v_min))
