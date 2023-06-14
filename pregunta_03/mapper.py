f52c852e7d28345e50d192197dd95ab3bdda4671
Kathe1995 <laacevedog@unal.edu.co>
2023-06-14 01:55:05 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,8 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-import sys 
-
-
-for line in sys.stdin:
-       sys.stdout.write(int(line.split(",")[1])+","+line.split(',')[0]+","+int(line.split(",")[1])+"\n")
+import sys
+for row in sys.stdin:
+  elementos = row.strip()
+  elementos = elementos.split(",")
+  
+  cantidad = elementos[1]
+  letra = elementos[0]
+  linea = letra + ";" + cantidad
+  sys.stdout.write(linea+"\n")
